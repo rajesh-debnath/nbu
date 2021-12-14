773b4f3b98f0e8cfccf2e244e0f4ab0e25c65337
sandipan1407 <sandipanpal8013678599@gmail.com>
2021-12-15 02:18:44 +0530

    starting stud_applicatin table

--- controller/stud_applicant.inc.php
+++ controller/stud_applicant.inc.php
@@ -10,4 +10,51 @@ if (isset($_POST["submit"])){
 	$semester =$_POST["semester"];
 	$mail =$_POST["email"];
 	$gender =$_POST["gender"];
+	$dob =$_POST["dob"];
+	$pwd =$_POST["pwd"];
+	$repwd =$_POST["repwd"];
+
+
+	include "connection.php";
+
+
+
+	if ((empty($name)||empty($pic)||empty($reg)||empty($session)||empty($course)||empty($class_roll)||empty($m_number)||empty($semester)||empty($mail)||empty($gender)||empty($dob)) !== false) {
+		
+
+		header("location: ../stud_applicant.php?error=emptyinput");
+		exit();
+	}
+	elseif (($pwd !== $repwd) !== false) {
+		header("location: ../stud_applicant.php?error=passwordsdonotmatch");
+		exit();
+	}
+	else {
+		$sqlins = "INSERT INTO stud_applicant (sa_profile_pic,	sa_name,	sa_gender,	sa_dob,	sa_reg_no,	sa_session,	sa_course,	sa_semester,	sa_class_roll,	sa_phn_no,	sa_mail	) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+		$stmt = mysqli_stmt_init($conn);
+		if (!mysqli_stmt_prepare($stmt, $sqlins)) {
+		  header("location: ../stud_applicant.php?error=stmtfailed");
+		  exit();
+	}
+
+
+	mysqli_stmt_bind_param($stmt, "sssssssssss", $uid, $fname, $lname, $age, $trainno, $doj, $board, $dest, $boardtime, $coach, $seat);
+	mysqli_stmt_execute($stmt);
+
+	mysqli_stmt_close($stmt);
+	header("location: ../ticket.php?error=none");
+	exit();
+	}
+
+    
+
+
+
+
+
+
+
+
+
+
 }
