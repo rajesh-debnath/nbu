9b9667f3f4221c13df1bf0e6715447fe8e4e15b7
rajesh-debnath <debnathrajesh6295@gmail.com>
2021-12-16 00:24:04 +0530

    some new file added

---
+++ admin_dashboard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Admin Dashboard</title>
+</head>
+<body>
+	<p>Student</p>
+	<ul>
+		<li>Pending Applicant</li>
+		<li>MSc. Computer science</li>
+		<li>MCA</li>
+	</li>
+	</ul>
+
+</body>
+</html>
---
+++ admin_login.php
@@ -0,0 +1,20 @@
+<?php include 'controller/connection.php';
+
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Admin Login</title>
+</head>
+<body>
+	<form method="post" action="controller/ad_login_verify.php">
+		<input type="text" name="ad_username">
+		<input type="password" name="ad_password">
+		<input type="submit" name="ad_submit">
+	</form>
+
+</body>
+</html>
